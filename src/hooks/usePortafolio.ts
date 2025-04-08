0f528de03a76cb9ff4a715f42888ac7bfdc7297f
Yandira Condo <116704694+Aria14cs@users.noreply.github.com>
2025-04-08 04:30:04 +0000

    actualizacion

--- src/hooks/usePortafolio.ts
+++ src/hooks/usePortafolio.ts
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import { portafolioData } from "../data/portafolioData";
 
 export const usePortafolio = () => {
-  const [data, setData] = useState(portafolioData);
+  const [data] = useState(portafolioData);
 
   useEffect(() => {
     document.title = `${data.nombre} - Portafolio`;
